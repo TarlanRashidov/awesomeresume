d57ee1f75b3eb4f1f6c6b95f9838836426530d7b
TarlanRashidov <rashidovterlan@hotmail.com>
2022-04-03 18:06:20 +0400

    Tarlan

--- 2/src/myfirstapp/MyFirstApp.java
+++ 2/src/myfirstapp/MyFirstApp.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication6;
+package myfirstapp;
 
 /**
  *
- * @author SAMSUNG
+ * @author Elvin Sadikh
  */
-public class JavaApplication6 {
+public class MyFirstApp {
 
     /**
      * @param args the command line arguments
